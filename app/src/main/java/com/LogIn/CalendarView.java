24ac96c07ed3e3d18b21bbe7a42e9ef5e575cf47
stevezhanggeek <stevezhanggeek@gmail.com>
2015-04-21 21:30:13 -0700

    Connect Real Data

--- app/src/main/java/com/LogIn/CalendarView.java
+++ app/src/main/java/com/LogIn/CalendarView.java
@@ -9,16 +9,18 @@ import android.graphics.drawable.ShapeDrawable;
 import android.graphics.drawable.shapes.RectShape;
 import android.util.AttributeSet;
 import android.view.View;
+import android.widget.ScrollView;
 
 import com.parse.FindCallback;
 import com.parse.ParseException;
 import com.parse.ParseObject;
 import com.parse.ParseQuery;
 
+import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
 
-public class CalendarView extends View {
+public class CalendarView extends ScrollView {
     private ShapeDrawable mDrawable;
     private int m_data;
     private List<ParseObject> m_valueList;
@@ -34,14 +36,11 @@ public class CalendarView extends View {
         super(context);
         m_data = temp;
 
-        System.out.println("Here");
-
         ParseQuery<ParseObject> query = ParseQuery.getQuery("LogIn");
         query.findInBackground(new FindCallback<ParseObject>() {
             public void done(List<ParseObject> valueList, ParseException e) {
                 if (e == null) {
                     m_valueList = valueList;
-                    System.out.println("Good");
                 } else {
                     System.out.println("WTH");
                 }
@@ -50,32 +49,46 @@ public class CalendarView extends View {
     }
 
     @Override
-    protected void onDraw(Canvas canvas) {
+    public void onDraw(Canvas canvas) {
         mCanvas = canvas;
         int width = mCanvas.getWidth();
         super.onDraw(canvas);
 
+        int textSize = 30;
         paint.setColor(Color.BLACK);
-        paint.setTextSize(30);
-        int start = 8;
+        paint.setTextSize(textSize);
+        int start_time_hour = 8;
+        int length_hour = 10;
         int hour_vertical_interval = 300;
-        int hour_width = 100;
+        int text_width = 100;
 
         paint.setAntiAlias(true);
-        for (int i = start; i < start+10; i++){
-            int y = (i - start + 1) * hour_vertical_interval;
-            canvas.drawText(i + ":00", 10, y, paint);
-            canvas.drawLine(hour_width, y-10, width, y-10, paint);
+        for (int i = start_time_hour; i < start_time_hour + length_hour; i++){
+            int y = (i - start_time_hour + 1) * hour_vertical_interval;
+            canvas.drawText(i + ":00", 10, y+textSize/2, paint);
+            canvas.drawLine(text_width, y, width, y, paint);
         }
 
         if (m_valueList != null) {
             for (ParseObject object: m_valueList) {
                 int value = object.getInt("value");
+                int startX = text_width + (value-1)*(width-text_width)/7;
+
                 Date time = object.getDate("time");
-                int startX = hour_width + (value-1)*(width-hour_width)/7;
-                canvas.drawRect(startX, hour_vertical_interval, startX + (width-hour_width)/7 - 10, hour_vertical_interval+10, paint);
+
+                Calendar cal = Calendar.getInstance();
+                cal.setTime(time);
+                int year = cal.get(Calendar.YEAR);
+                int month = cal.get(Calendar.MONTH);
+                int day = cal.get(Calendar.DAY_OF_MONTH);
+                int hour = cal.get(Calendar.HOUR);
+                double minute = cal.get(Calendar.MINUTE);
+                System.out.println(this.getHeight());
+
+                int startY = hour_vertical_interval + (hour-start_time_hour+(int)(minute/60)) * hour_vertical_interval;
+
+                canvas.drawRect(startX, startY, startX + (width-text_width)/7 - 10, startY+20, paint);
             }
         }
-//        canvas.drawRect(hour_width, hour_vertical_interval, hour_width + (width-hour_width)/7, hour_vertical_interval+10, paint);
     }
 }
--- app/src/main/java/com/LogIn/MainActivity.java
+++ app/src/main/java/com/LogIn/MainActivity.java
@@ -19,21 +19,25 @@ package com.LogIn;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentTransaction;
 
-/**
- * Base launcher activity, to handle most of the common plumbing for samples.
- */
-public class VisualizationActivity extends FragmentActivity {
+import com.parse.Parse;
 
-    public static final String TAG = "SampleActivityBase";
+public class MainActivity extends FragmentActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-    }
+        setContentView(R.layout.activity_main);
+        startActivity(new Intent(this, StartLockScreen.class));
 
-    @Override
-    protected void onStart() {
-        super.onStart();
+        if (savedInstanceState == null) {
+            FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+            SlidingTabsBasicFragment fragment = new SlidingTabsBasicFragment();
+            transaction.replace(R.id.sample_content_fragment, fragment);
+            transaction.commit();
+        }
+        Parse.enableLocalDatastore(getApplicationContext());
+        Parse.initialize(this, "9DNSMkDuMcOv0Mi918JSe1CfMlkBPQ9UJVp8ksQB", "Qo53m1lBF7kXzbdP0OJ8bbL1OH6AuJnZbFRyOI4K");
     }
 }
